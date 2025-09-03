bade859c10c419196fecef3cf5e044c943852ecf
praveenkumarpentamalla <praveenkumarpentamalla@gmail.com>
2025-09-03 15:44:19 +0530

    Updated landing page test cases

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -10,8 +10,22 @@ class LandingPage(Base):
 
     def landing(self):
         self.driver.get("https://civilbook.in")
+        print("Home URL:", self.driver.current_url)
+        print("Home Title:", self.driver.title)
+
         self.click(self.home_button)
+        print("Home URL:", self.driver.current_url)
+        print("Home Title:", self.driver.title)
+
         self.click(self.departments_button)
+        print("Home URL:", self.driver.current_url)
+        print("Home Title:", self.driver.title)
+
         self.click(self.about_button)
+        print("Home URL:", self.driver.current_url)
+        print("Home Title:", self.driver.title)
+        
         self.click(self.contact_button)
+        print("Home URL:", self.driver.current_url)
+        print("Home Title:", self.driver.title)
 
